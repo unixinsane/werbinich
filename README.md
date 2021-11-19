b593d41d3e709cdcbe69002fa650c56abfe28375
unix <giftgamer0@gmail.com>
2021-11-20 00:53:43 +0300

    1.0.0
    Developers : unixinsane - kwainsane

---
+++ README.md
@@ -0,0 +1,58 @@
+# WER BİN İCH 
+
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/a578dde078ef481e97a0e7eac0c8d312)](https://app.codacy.com/app/Mebus/cupp?utm_source=github.com&utm_medium=referral&utm_content=Mebus/cupp&utm_campaign=Badge_Grade_Dashboard)
+
+## Update
+
+Brute Force Kısmı Eklendi
+Termuxla Uyumlu Halide Geldi 
+
+
+## Requirements
+------------
+Çalışması İcin python3 Gereklidir
+
+Hata Alırsanız
+-----------
+İnstax Hatası İcin
+```
+cd Tools/İnstax
+chmod +x install.sh
+bash install.sh
+chmod +x instax.sh
+service tor start
+cd ..
+```
+Termuxta Nmap Hatası İcin 
+```
+Önce İşlem Numarasını 0 Yapın Ondan Sonra Sıkıntı Olmıyacaktır
+```
+
+
+Linux Quick start
+-----------
+
+    $ python3 werbinich.py 
+    
+    
+Linux Görüntüsü
+-----------
+![Linux](https://user-images.githubusercontent.com/81394274/113740807-b5786c00-9709-11eb-8ee2-d4e75953ce4e.png)
+    
+Termux Quick start
+-----------
+
+    $ python3 werbinich-termux.py 
+    
+    
+Termux Görüntüsü
+-----------
+![hWjZC5](https://user-images.githubusercontent.com/81394274/113740290-3e42d800-9709-11eb-8692-bb67c350d2d5.png)
+
+
+
+
+
+
+
+
---
+++ werbinich-termux.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import os
+
+os.system("apt-get install figlet")
+os.system("clear")
+os.system("figlet WER   BIN   ICH?")
+print("""
+
+\033[31m\033[1mHata Almamak İcin İlk Önce Numarayı 0 Yapın
+
+\033[97m----------------------------------------------------------------------------------------------
+|                                   |                           |                            |
+| \033[92mNmap Kısayolları                  \033[97m|  \033[92mWorldlist Kısayolları    \033[97m|  \033[92mBrute Force Kısayolları   \033[97m|            
+|                                   \033[97m|                           |                            |
+| \033[94m[\033[97m01\033[94m] Hizli Tarama                 \033[97m|  \033[94m[\033[97m04\033[94m] Cupp.py Başlatma    \033[97m|  \033[94m[\033[97m06\033[94m] İnstax Başlatma\033[97m[\033[31mRoot\033[97m]\033[97m|   
+| \033[94m[\033[97m02\033[94m] Servis Ve Versiyon Bilgisi   \033[97m|                           |                            |
+| \033[94m[\033[97m03\033[94m] Isletım Sıstemı Bilgisi      \033[97m|  \033[92mFake Site Script         \033[97m|                            |
+|                                   \033[97m|                           \033[97m|                            |
+|                                   |  \033[94m[\033[97m05\033[94m] Blackeye Başlatma   \033[97m|                            |
+|                                   |                           |                            |
+|                                   |                           |                            |  
+---------------------------------------------------------------------------------------------- 
+| \033[92mDDOS Attack Kısayolları           \033[97m|
+\033[97m|                                   |
+\033[97m| \033[94m[\033[97m07\033[94m] DDOS Saldırı Kontrolü\033[97m[\033[31mRoot\033[97m]        \033[97m| 
+\033[97m|                                   | 
+|                                   |
+|                                   |
+|                                   |
+|                                   |
+|                                   |
+|                                   |
+-------------------------------------
+""")
+
+islemno = input("\033[37mIslem Numarası Giriniz: ")
+
+if(islemno=="1"):
+               hedefip = input("\033[37mHedef İp Girin: ")
+               os.system("nmap " + hedefip)
+elif(islemno=="2"):
+               hedefip = input("\033[37mHedef İp Girin: ")
+               os.system("nmap -sS -sV " + hedefip)
+elif(islemno=="3"):
+               hedefip = input("\033[37mHedef İp Girin: ")
+               os.system("nmap -o " + hedefip)
+elif(islemno=="4"):
+               os.system("python3 Tools/Cupp.py/cupp.py -i")
+elif(islemno=="5"):
+               os.system("bash Tools/Blackeye/blackeye.sh")
+elif(islemno=="6"):
+               os.system("bash Tools/İnstax/instax.sh")
+elif(islemno=="7"):
+               hedefip = input("\033[37mHedef İp Girin: ")
+               os.system("tcpdump host " + hedefip)
+elif(islemno=="0"):
+               os.system("pkg install nmap")
+               os.system("pkg install tcpdump")
+else:
+  print("\033[31mHatalı Secim Yaptın")
+
+
